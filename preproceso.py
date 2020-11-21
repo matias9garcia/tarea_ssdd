21ebdd33b46ac0794aa8bd402d7d70ceb77fdc11
Arvaloplz <alvaro.andrade.a@mail.pucv.cl>
2020-11-21 14:00:15 -0300

    bipolar

--- preproceso.py
+++ preproceso.py
@@ -13,10 +13,10 @@ data_kddtrain = pd.read_csv("KDDTrain+_20Percent.txt", header=None)
 
 "Se transforman los atributos no númericos, utilizando labelEncoder"
 X_train = data_kddtrain.drop([42], axis=1)
-y_train = data_kddtrain[42] #Etiqueta
+etiqueta_train = data_kddtrain[41] #Etiqueta
 
 X_test = data_kddtest.drop([42], axis=1)
-y_test = data_kddtest[42] #Etiqueta
+etiqueta_test = data_kddtest[41] #Etiqueta
 
 label_train=LabelEncoder()
 for c in  X_train.columns:
@@ -32,6 +32,22 @@ for c in  X_test.columns:
     else:
         X_test[c]=X_test[c]
 
+etiqueta_test_bipolar = []
+for etiqueta in etiqueta_test:
+    if  etiqueta == "normal":
+        y = 1
+    else :
+        y = -1
+    etiqueta_test_bipolar.append(y)
+
+etiqueta_train_bipolar = []
+for etiqueta in etiqueta_train:
+    if etiqueta == "normal":
+        y = 1
+    else:
+        y = -1
+    etiqueta_train_bipolar.append(y)
+
 
 "Normalización de la data"
 train_std = (X_train - X_train.min(axis=0)) / (X_train.max(axis=0) - X_train.min(axis=0))
@@ -43,3 +59,6 @@ test_normalize = test_std * (0.99 - 0.1) + 0.1
 
 train_normalize.to_csv(r'train.txt', header=None, index=None, sep=' ', mode='a')
 test_normalize.to_csv(r'test.txt', header=None, index=None, sep=' ', mode='a')
+
+print(etiqueta_test_bipolar)
+print(etiqueta_train_bipolar)
