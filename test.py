5b690daaa7322ebe4db09a4a2d5b00a232dd27d4
Arvaloplz <alvaro.andrade.a@mail.pucv.cl>
2020-11-22 00:06:01 -0300

    ✨ pseudocodigo

---
+++ test.py
@@ -0,0 +1,8 @@
+# Load test_input y test_label  # Data de testing
+# Load pesos  # Pesos estrenados
+# # Calcular data de salida del IDS
+# z = forward(Xv, w1, bias, w2)
+# # Calcular métrica de rendimiento
+# [accuracy, Fscore] = metrica(z, Yv)
+# #Grabar Exactitud y r F-score de cada Clase
+# savetxt(‘fscore.csv’, Accuracy, Fscore)
